7c4fbc674e400f044bbd7c80fd6c0a26b9cf0e15
David Loake <arcturustestecs@gmail.com>
2023-04-04 20:54:40 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ecs-ga-library-v2`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
